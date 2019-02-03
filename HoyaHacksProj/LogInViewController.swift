0b1752a24f92dfa37d7fe210da3200929741df55
Elias Aleman <aleman.music@gmail.com>
2019-02-02 22:54:09 -0500

    3rd commit

--- HoyaHacksProj/LogInViewController.swift
+++ HoyaHacksProj/LogInViewController.swift
@@ -7,13 +7,31 @@
 //
 
 import UIKit
+import Firebase
 
 
 class LogInScreenViewController: UIViewController {
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
     
     @IBAction func GoButtonPressed(_ sender: Any) {
         print("Go button pressed.")
+        Auth.auth().signIn(withEmail: email.text!, password: password.text!) { (user, error) in
+            if error == nil{
+                self.performSegue(withIdentifier: "loginToHome", sender: self)
+            }
+            else{
+                let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                
+                alertController.addAction(defaultAction)
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
+        
     }
+        
+ 
     @IBAction func BackButtonPressed(_ sender: Any) {
         print("Back button pressed.")
     }
--- HoyaHacksProj/SignUpViewController.swift
+++ HoyaHacksProj/SignUpViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import Firebase
 
 
 class SignUpScreenViewController: UIViewController {
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var phone: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var passwordConfirm: UITextField!
     
     @IBAction func BackButtonPressed(_ sender: Any) {
         print("Back button pressed.")
@@ -17,7 +23,29 @@ class SignUpScreenViewController: UIViewController {
     
     @IBAction func SaveInfoPressed(_ sender: Any) {
         print("Save Info button pressed.")
+        if password.text != passwordConfirm.text {
+            let alertController = UIAlertController(title: "Password Incorrect", message: "Please re-type password", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else{
+            Auth.auth().createUser(withEmail: email.text!, password: password.text!){ (user, error) in
+                if error == nil {
+                    self.performSegue(withIdentifier: "signupToHome", sender: self)
+                }
+                else{
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
     }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
